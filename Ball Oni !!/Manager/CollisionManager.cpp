2f2aba16d6a32ea069dd18cfb0ebfe7ab72abda8
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-03-04 15:51:05 +0900

    衝突判定を管理するクラスを作成

---
+++ Ball Oni !!/Manager/CollisionManager.cpp
@@ -0,0 +1,8 @@
+﻿//
+//	File	 : CollisionManager.cpp
+//
+//  Contents : 衝突時の管理クラス定義
+//
+//	name	 : Hibiki Yoshiyasu
+//
+#include "CollisionManager.h"
---
+++ Ball Oni !!/Manager/CollisionManager.h
@@ -0,0 +1,13 @@
+﻿//
+//	File	 : CollisionManager.h
+//
+//  Contents : 衝突時の管理クラス宣言
+//
+//	name	 : Hibiki Yoshiyasu
+//
+#pragma once
+
+class CollisionManager
+{
+
+};
