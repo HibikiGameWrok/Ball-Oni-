36c1b54ee4ae38363dd4015f4efb7490ba680a6d
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-03-16 01:07:51 +0900

    ゲームが始まる前に表示する説明を表示する画像と表示する為のクラスファイルを追加

---
+++ Ball Oni !!/GameObj/UI/TeachPlayStartUI.cpp
@@ -0,0 +1,45 @@
+﻿#include "TeachPlayStartUI.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+TeachPlayStartUI::TeachPlayStartUI()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+TeachPlayStartUI::~TeachPlayStartUI()
+{
+}
+
+/// <summary>
+/// 開始処理
+/// </summary>
+void TeachPlayStartUI::Start()
+{
+	System::DrawManager::GetInstance().LoadTexture(m_imageTeachPlayStartUI, L"Resources\\Textures\\PlayScene\\TeachPlayStart.png");
+	m_imageTeachPlayStartUI.SetRect(600, 200);
+	m_imageTeachPlayStartUI.SetOrigin(600 / 2, 200 / 2);
+	m_imageTeachPlayStartUI.SetPos(800 / 2, 250);
+}
+
+/// <summary>
+/// 更新処理
+/// </summary>
+/// <returns>true == 更新, false == 停止 </returns>
+bool TeachPlayStartUI::Update(float elapsedTime)
+{
+
+	return true;
+}
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void TeachPlayStartUI::Draw()
+{
+	System::DrawManager::GetInstance().Draw(m_imageTeachPlayStartUI);
+}
+
---
+++ Ball Oni !!/GameObj/UI/teachplaystartui.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "../../TaskSystem/TaskBase.h"
+#include "../../Manager/DrawManager.h"
+
+class TeachPlayStartUI : public Task::TaskBase
+{
+private:
+	System::DrawData m_imageTeachPlayStartUI;
+
+public:
+	// コンストラクタ
+	TeachPlayStartUI();
+	// デストラクタ
+	~TeachPlayStartUI();
+
+	// 初期化
+	void Start();
+	// 更新処理
+	bool Update(float elapsedTime);
+
+	// 描画
+	void Draw();
+};
