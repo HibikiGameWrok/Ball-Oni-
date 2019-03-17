dc6817f50b35d846b53f31fd9996e48df6809a2d
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-03-18 03:04:16 +0900

    ３カウントダウンする処理を別のファイルに移行し、処理の部分を削除

--- Ball Oni !!/GameObj/UI/GameTime.cpp
+++ Ball Oni !!/GameObj/UI/GameTime.cpp
@@ -19,8 +19,6 @@ GameTime::GameTime()
 	,m_finishFlag(false)
 	,m_framesTime(60)
 	,m_secondsTime(60)
-	//,m_countDown_rect(0,0,100,100)
-	//,m_countDown_scale(MAX_COUNTDOWNUI_SIZE)
 {
 }
 
@@ -36,18 +34,6 @@ GameTime::~GameTime()
 /// </summary>
 void GameTime::Start()
 {
-	//// カウントダウンするUI
-	//System::DrawManager::GetInstance().LoadTexture(m_countDownUI, L"Resources\\Textures\\PlayScene\\CountDown.png");
-	//m_countDownUI.SetRect(
-	//	static_cast<LONG>(m_countDown_rect.x),
-	//	static_cast<LONG>(m_countDown_rect.y),
-	//	static_cast<LONG>(m_countDown_rect.z),
-	//	static_cast<LONG>(m_countDown_rect.w)
-	//);
-	//m_countDownUI.SetOrigin(100 / 2, 100 / 2);
-	//m_countDownUI.SetPos(800 / 2, 600 /2);
-	//m_countDownUI.SetScale(m_countDown_scale.x, m_countDown_scale.y);
-
 	// タイムの背景
 	System::DrawManager::GetInstance().LoadTexture(m_backGround, L"Resources\\Textures\\PlayScene\\Time_Back.png");
 	m_backGround.SetRect(40, 40);
@@ -55,12 +41,12 @@ void GameTime::Start()
 
 	// フレーム数
 	System::DrawManager::GetInstance().LoadTexture(m_onePlace, L"Resources\\Textures\\PlayScene\\time.png");
-	m_onePlace.SetRect(m_secondsTime / 10 * TimeUI_WIDTH,0, m_secondsTime / 10 * TimeUI_WIDTH + TimeUI_WIDTH, TimeUI_HEIGHT);
+	m_onePlace.SetRect(m_secondsTime / 10 * TIME_UI_WIDTH,0, m_secondsTime / 10 * TIME_UI_WIDTH + TIME_UI_WIDTH, TIME_UI_HEIGHT);
 	m_onePlace.SetPos(390, 0);
 
 	// 秒数
 	System::DrawManager::GetInstance().LoadTexture(m_tenPlace, L"Resources\\Textures\\PlayScene\\time.png");
-	m_tenPlace.SetRect(m_secondsTime % 10 * TimeUI_WIDTH,0, m_secondsTime % 10 * TimeUI_WIDTH + TimeUI_WIDTH, TimeUI_HEIGHT);
+	m_tenPlace.SetRect(m_secondsTime % 10 * TIME_UI_WIDTH,0, m_secondsTime % 10 * TIME_UI_WIDTH + TIME_UI_WIDTH, TIME_UI_HEIGHT);
 	m_tenPlace.SetPos(410, 0);
 
 	// 終了画像
@@ -81,21 +67,12 @@ bool GameTime::Update(float elapsedTime)
 	{
 		// タイム処理
 		Count_Time();
-		// カウントダウン処理
-		CountDown();
 	}
 
-	//// 毎フレーム大きさと画像サイズを更新
-	//m_countDownUI.SetScale(m_countDown_scale.x, m_countDown_scale.y);
-	//m_countDownUI.SetRect(
-	//	static_cast<LONG>(m_countDown_rect.x),
-	//	static_cast<LONG>(m_countDown_rect.y),
-	//	static_cast<LONG>(m_countDown_rect.z),
-	//	static_cast<LONG>(m_countDown_rect.w)
-	//	);
-
-	m_onePlace.SetRect(m_secondsTime / 10 * TimeUI_WIDTH, 0, m_secondsTime / 10 * TimeUI_WIDTH + TimeUI_WIDTH, TimeUI_HEIGHT);
-	m_tenPlace.SetRect(m_secondsTime % 10 * TimeUI_WIDTH, 0, m_secondsTime % 10 * TimeUI_WIDTH + TimeUI_WIDTH, TimeUI_HEIGHT);
+	// 1桁目の表示範囲を設定
+	m_onePlace.SetRect(m_secondsTime / 10 * TIME_UI_WIDTH, 0, m_secondsTime / 10 * TIME_UI_WIDTH + TIME_UI_WIDTH, TIME_UI_HEIGHT);
+	// 10桁目の表示範囲を設定
+	m_tenPlace.SetRect(m_secondsTime % 10 * TIME_UI_WIDTH, 0, m_secondsTime % 10 * TIME_UI_WIDTH + TIME_UI_WIDTH, TIME_UI_HEIGHT);
 	return true;
 }
 
@@ -104,11 +81,6 @@ bool GameTime::Update(float elapsedTime)
 /// </summary>
 void GameTime::Draw()
 {
-	//if ((m_secondsTime <= 3 && m_secondsTime >= 1) && m_secondsTime > 0)
-	//{
-	//	System::DrawManager::GetInstance().Draw(m_countDownUI);
-	//}
-
 	// 背景
 	System::DrawManager::GetInstance().Draw(m_backGround);
 
@@ -149,54 +121,3 @@ void GameTime::Count_Time()
 		}
 }
 
-/// <summary>
-/// 終了間際の3秒カウントダウン
-/// </summary>
-void GameTime::CountDown()
-{
-	//// 画像スケールが0になった時大きい状態に戻す
-	//if (m_countDown_scale.x <= 0.0f)
-	//{
-	//	m_countDown_scale.x = MAX_COUNTDOWNUI_SIZE;
-	//	m_countDown_scale.y = MAX_COUNTDOWNUI_SIZE;
-	//}
-
-	//// 残り３秒の時
-	//if (m_secondsTime == 3)
-	//{
-	//	// 画像スケールを縮小
-	//	if (m_countDown_scale.x > 0 || m_countDown_scale.y > 0)
-	//	{
-	//		m_countDown_scale.x -= 0.1f;
-	//		m_countDown_scale.y -= 0.1f;
-	//	}
-	//}
-	//// 残り２秒の時
-	//if (m_secondsTime == 2)
-	//{
-	//	m_countDown_rect.x = COUNTUI_WIDTH;
-	//	m_countDown_rect.y = 0;
-	//	m_countDown_rect.z = COUNTUI_WIDTH * 2;
-	//	m_countDown_rect.w = COUNTUI_WIDTH;
-	//	// 画像スケールを縮小
-	//	if (m_countDown_scale.x > 0 || m_countDown_scale.y > 0)
-	//	{
-	//		m_countDown_scale.x -= 0.1f;
-	//		m_countDown_scale.y -= 0.1f;
-	//	}
-	//}
-	//// 残り１秒の時
-	//if (m_secondsTime == 1)
-	//{
-	//	m_countDown_rect.x = COUNTUI_WIDTH * 2;
-	//	m_countDown_rect.y = 0;
-	//	m_countDown_rect.z = COUNTUI_WIDTH * 3;
-	//	m_countDown_rect.w = COUNTUI_WIDTH;
-	//	// 画像スケールを縮小
-	//	if (m_countDown_scale.x > 0 || m_countDown_scale.y > 0)
-	//	{
-	//		m_countDown_scale.x -= 0.1f;
-	//		m_countDown_scale.y -= 0.1f;
-	//	}
-	//}
-}
--- Ball Oni !!/GameObj/UI/gametime.h
+++ Ball Oni !!/GameObj/UI/gametime.h
@@ -8,15 +8,10 @@ class GameTime : public Task::TaskBase
 public:
 	const int MAX_FRAME_TIME = 60;	// 最大のフレームタイム
 
-	const float MAX_COUNTDOWNUI_SIZE = 6.0f;	// カウントダウンするUIの全体サイズ
-
-	const float COUNTUI_WIDTH = 100.0f;		// 3秒カウントダウンするUIの幅	
-
-	const int TimeUI_WIDTH = 20;	// 画像の１つの数字分の幅
-	const int TimeUI_HEIGHT = 40;	// 画像の１つの数字分の高さ
+	const int TIME_UI_WIDTH = 20;	// 画像の１つの数字分の幅
+	const int TIME_UI_HEIGHT = 40;	// 画像の１つの数字分の高さ
 	
 private:
-	//System::DrawData m_countDownUI;	// 開始と終了時にカウントするUI
 	System::DrawData m_backGround;	// 背景
 	System::DrawData m_onePlace;		// フレーム単位画像
 	System::DrawData m_tenPlace;		// 秒単位画像
@@ -27,9 +22,6 @@ private:
 
 	int m_framesTime;  // フレーム単位 カウント用変数
 	int m_secondsTime; // 秒単位	   カウント用変数
-
-	//DirectX::SimpleMath::Vector4 m_countDown_rect;  // カウントダウンUIの画像範囲
-	//DirectX::SimpleMath::Vector2 m_countDown_scale;	// カウントダウンUIの大きさを変動させる変数
 
 public:
 	// コンストラクタ
@@ -49,33 +41,19 @@ public:
 	// タイムのカウントダウン処理
 	void Count_Time();
 
-	// カウントダウン
-	void CountDown();
 
 public:
 	// フレームカウントを取得
-	int GetFrameTime() {
-		return m_framesTime;
-	}
-
+	int GetFrameTime() { return m_framesTime; }
 	// 秒を取得
-	int GetSecondsTime() {
-		return m_secondsTime;
-	}
+	int GetSecondsTime() { return m_secondsTime; }
 	
 	// カウントを止めるフラグ
-	bool GetStopFlag() {
-		return m_stopFlag;
-	}
-
+	bool GetStopFlag() { return m_stopFlag; }
 	// 終了フラグを設定
-	void SetStopFlag(bool stop) {
-		m_stopFlag = stop;
-	}
-	// 終了フラグを取得
-	bool GetFinishFlag() {
-		return m_finishFlag;
-	}
+	void SetStopFlag(bool stop) { m_stopFlag = stop; }
 
+	// 終了フラグを取得
+	bool GetFinishFlag() { return m_finishFlag; }
 };
 
