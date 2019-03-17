8d5488495f1166b5fa6891b02ed4ea4bb455d70c
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-03-18 03:59:58 +0900

    「スタート」合図する画像の描画設定を追加

--- Ball Oni !!/GameObj/UI/StartSign.cpp
+++ Ball Oni !!/GameObj/UI/StartSign.cpp
@@ -1,1 +1,42 @@
-﻿
+﻿#include "StartSign.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+StartSignUI::StartSignUI()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+StartSignUI::~StartSignUI()
+{
+}
+
+/// <summary>
+/// 開始処理
+/// </summary>
+void StartSignUI::Start()
+{
+	System::DrawManager::GetInstance().LoadTexture(m_imageStartSignUI, L"Resources\\Textures\\PlayScene\\StartSign.png");
+	m_imageStartSignUI.SetRect(550, 150);
+	m_imageStartSignUI.SetOrigin(550 / 2, 150 / 2);
+	m_imageStartSignUI.SetPos(800 / 2, 600 / 2);
+}
+
+/// <summary>
+/// 更新処理
+/// </summary>
+bool StartSignUI::Update(float elapsedTime)
+{
+	return true;
+}
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void StartSignUI::Draw()
+{
+	System::DrawManager::GetInstance().Draw(m_imageStartSignUI);
+}
--- Ball Oni !!/GameObj/UI/StartSign.h
+++ Ball Oni !!/GameObj/UI/StartSign.h
@@ -1,1 +1,24 @@
 ﻿#pragma once
+
+#include "../../TaskSystem/TaskBase.h"
+#include "../../Manager/DrawManager.h"
+
+class StartSignUI : public Task::TaskBase
+{
+private:
+	System::DrawData m_imageStartSignUI;
+
+public:
+	// コンストラクタ
+	StartSignUI();
+	// デストラクタ
+	~StartSignUI();
+
+	// 初期化
+	void Start();
+	// 更新処理
+	bool Update(float elapsedTime);
+
+	// 描画
+	void Draw();
+};
