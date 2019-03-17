7afca869942f211786de2f98f36f91747d5275d5
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-03-18 03:02:36 +0900

    描画座標と、1フレーム1カウントダウンする処理を追加

--- Ball Oni !!/GameObj/UI/CountDownUI.cpp
+++ Ball Oni !!/GameObj/UI/CountDownUI.cpp
@@ -4,9 +4,26 @@
 /// コンストラクタ
 /// </summary>
 CountDownUI::CountDownUI()
-	:m_countDown_rect(0,0,100,100)
+	:m_frameTime(0)
+	,m_countTime(MAX_COUNT)
+	,m_countDown_rect(0,0,100,100)
 	,m_countDown_scale(MAX_COUNTDOWNUI_SIZE)
+	,m_moveFlag(false)
 {
+}
+
+/// <summary>
+/// 引数ありコンストラクタ
+/// </summary>
+/// <param name="pos">描画座標</param>
+CountDownUI::CountDownUI(DirectX::SimpleMath::Vector2 pos)
+	:m_frameTime(0)
+	, m_countTime(MAX_COUNT)
+	, m_countDown_rect(0, 0, 100, 100)
+	, m_countDown_scale(MAX_COUNTDOWNUI_SIZE)
+	, m_moveFlag(false)
+{
+	m_pos = pos;
 }
 
 /// <summary>
@@ -23,14 +40,19 @@ void CountDownUI::Start()
 {
 	// カウントダウンするUI
 	System::DrawManager::GetInstance().LoadTexture(m_imageCountDownUI, L"Resources\\Textures\\PlayScene\\CountDown.png");
+	// 画像の表示範囲を設定
 	m_imageCountDownUI.SetRect(
 		static_cast<LONG>(m_countDown_rect.x),
 		static_cast<LONG>(m_countDown_rect.y),
 		static_cast<LONG>(m_countDown_rect.z),
 		static_cast<LONG>(m_countDown_rect.w)
 	);
+
+	// 画像の中心点を設定
 	m_imageCountDownUI.SetOrigin(100 / 2, 100 / 2);
-	m_imageCountDownUI.SetPos(800 / 2, 600 / 2);
+	// 画像の座標を設定
+	m_imageCountDownUI.SetPos(m_pos.x, m_pos.y);
+	// 画像の全体サイズの設定
 	m_imageCountDownUI.SetScale(m_countDown_scale.x, m_countDown_scale.y);
 }
 
@@ -40,6 +62,21 @@ void CountDownUI::Start()
 /// <returns>true == 更新, false == 停止 </returns>
 bool CountDownUI::Update(float elapsedTime)
 {
+	// 動作フラグがtrueの時
+	if (m_moveFlag == true)
+	{
+		CountDownFunc();
+	}
+	else
+	{
+		// 初期化
+		m_frameTime = 0;
+		m_countTime = MAX_COUNT;
+	}
+
+	// 画像の挙動処理
+	ImageMove();
+
 	// 毎フレーム大きさと画像サイズを更新
 	m_imageCountDownUI.SetScale(m_countDown_scale.x, m_countDown_scale.y);
 	m_imageCountDownUI.SetRect(
@@ -56,9 +93,31 @@ bool CountDownUI::Update(float elapsedTime)
 /// </summary>
 void CountDownUI::Draw()
 {
-	if ((m_countTime <= 3 && m_countTime >= 1) && m_countTime > 0)
+	// ３～１の間に描画をし続ける
+	if (m_moveFlag == true && ((m_countTime <= 3 && m_countTime >= 1) && m_countTime > 0))
 	{
 		System::DrawManager::GetInstance().Draw(m_imageCountDownUI);
+	}
+}
+
+/// <summary>
+/// カウントダウン処理
+/// </summary>
+void CountDownUI::CountDownFunc()
+{
+	// フレームタイムを60fになるまで加算
+	m_frameTime += 1;
+	if (m_frameTime >= 60)
+	{
+		// 1秒引く
+		m_countTime -= 1;
+		// 初期化
+		m_frameTime = 0;
+	}
+
+	if (m_countTime <= 0)
+	{
+		m_moveFlag = false;
 	}
 }
 
@@ -68,7 +127,7 @@ void CountDownUI::Draw()
 void CountDownUI::ImageMove()
 {
 	// 画像スケールが0になった時大きい状態に戻す
-	if (m_countDown_scale.x <= 0.0f)
+	if (m_countDown_scale.x <= ZERO_SCALE)
 	{
 		m_countDown_scale.x = MAX_COUNTDOWNUI_SIZE;
 		m_countDown_scale.y = MAX_COUNTDOWNUI_SIZE;
@@ -78,38 +137,42 @@ void CountDownUI::ImageMove()
 	if (m_countTime == 3)
 	{
 		// 画像スケールを縮小
-		if (m_countDown_scale.x > 0 || m_countDown_scale.y > 0)
+		if (m_countDown_scale.x > ZERO_SCALE || m_countDown_scale.y > ZERO_SCALE)
 		{
-			m_countDown_scale.x -= 0.1f;
-			m_countDown_scale.y -= 0.1f;
+			m_countDown_scale.x -= SMALL_DOWN;
+			m_countDown_scale.y -= SMALL_DOWN;
 		}
 	}
+
 	// 残り２秒の時
 	if (m_countTime == 2)
 	{
+		// 描画範囲をずらす
 		m_countDown_rect.x = COUNTUI_WIDTH;
 		m_countDown_rect.y = 0;
 		m_countDown_rect.z = COUNTUI_WIDTH * 2;
 		m_countDown_rect.w = COUNTUI_WIDTH;
 		// 画像スケールを縮小
-		if (m_countDown_scale.x > 0 || m_countDown_scale.y > 0)
+		if (m_countDown_scale.x > ZERO_SCALE || m_countDown_scale.y > ZERO_SCALE)
 		{
-			m_countDown_scale.x -= 0.1f;
-			m_countDown_scale.y -= 0.1f;
+			m_countDown_scale.x -= SMALL_DOWN;
+			m_countDown_scale.y -= SMALL_DOWN;
 		}
 	}
+
 	// 残り１秒の時
 	if (m_countTime == 1)
 	{
+		// 描画範囲をずらす
 		m_countDown_rect.x = COUNTUI_WIDTH * 2;
 		m_countDown_rect.y = 0;
 		m_countDown_rect.z = COUNTUI_WIDTH * 3;
 		m_countDown_rect.w = COUNTUI_WIDTH;
 		// 画像スケールを縮小
-		if (m_countDown_scale.x > 0 || m_countDown_scale.y > 0)
+		if (m_countDown_scale.x > ZERO_SCALE || m_countDown_scale.y > ZERO_SCALE)
 		{
-			m_countDown_scale.x -= 0.1f;
-			m_countDown_scale.y -= 0.1f;
+			m_countDown_scale.x -= SMALL_DOWN;
+			m_countDown_scale.y -= SMALL_DOWN;
 		}
 	}
 }
--- Ball Oni !!/GameObj/UI/CountDownUI.h
+++ Ball Oni !!/GameObj/UI/CountDownUI.h
@@ -6,23 +6,31 @@
 class CountDownUI : public Task::TaskBase
 {
 public:
+	const int MAX_COUNT = 3;
+
 	const float MAX_COUNTDOWNUI_SIZE = 6.0f;	// カウントダウンするUIの全体サイズ
 
-	const float COUNTUI_WIDTH = 100.0f;		// 3秒カウントダウンするUIの幅	
+	const float ZERO_SCALE = 0.0f;	// scaleが0の定数
 
-	const int TimeUI_WIDTH = 20;	// 画像の１つの数字分の幅
-	const int TimeUI_HEIGHT = 40;	// 画像の１つの数字分の高さ
+	const float SMALL_DOWN = 0.1f;  // 小さくする数値 
+
+	const float COUNTUI_WIDTH = 100.0f;		// 3秒カウントダウンするUIの幅	
 private:
 	System::DrawData m_imageCountDownUI;
 	
+	int m_frameTime; // フレームタイム
 	int m_countTime; // カウントタイム
 
+	DirectX::SimpleMath::Vector2 m_pos;	// 描画座標
 	DirectX::SimpleMath::Vector4 m_countDown_rect;  // カウントダウンUIの画像範囲
 	DirectX::SimpleMath::Vector2 m_countDown_scale;	// カウントダウンUIの大きさを変動させる変数
+
+	bool m_moveFlag;  // UIが動作中か判断するフラグ
 
 public:
 	// コンストラクタ
 	CountDownUI();
+	CountDownUI(DirectX::SimpleMath::Vector2 pos);
 	// デストラクタ
 	~CountDownUI();
 
@@ -34,6 +42,13 @@ public:
 	// 描画
 	void Draw();
 
+	// カウントダウン処理
+	void CountDownFunc();
+
 	// 画像の挙動処理
 	void ImageMove();
+public:
+	// 動作フラグを取得&設定する関数
+	bool GetMoveFlag() { return m_moveFlag; }
+	void SetMoveFlag(bool moveFlag) { m_moveFlag = moveFlag; }
 };
